b3ade458dce03056add7558736e64138678c2171
ok09ra <okudasota@gmail.com>
2022-01-14 15:17:31 +0900

    ページを作成

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <dev id="Head">
+        <h1 id="ProjectTitle">GENE SLIME MOLD</h1>
+        <p id="UserAddress">ADDRESS</p>
+        <nuxt-link to="/holder" > ホルダーページ</nuxt-link>
+        <nuxt-link to="/usereventmaker" > ユーズイベントメイカーページ</nuxt-link>
+        <nuxt-link to="/miner" > マイナーページ</nuxt-link>
+    </dev>
+</template>
+
+<style>
+    #Head {
+        position: absolute;
+        background-color: black;
+    }
+    #ProjectTitle {
+        color: red;
+    }
+
+    #UserAddress {
+        color: blue;
+        position: abusolute;
+        right: 50px;
+    }
+
+</style>
